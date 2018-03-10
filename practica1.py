f124e0d032d9ee0b320a23a7cc10e7f1f3d9bcc7
Rodrigo_P <r.pachecom@alumnos.urjc.es>
2018-03-10 17:44:28 +0100

    Minor bug fixed. Not taking into account main resource

--- practica1.py
+++ practica1.py
@@ -15,26 +15,26 @@ class URL_shortener(webapp.webApp):
         try:
             method = request.split()[0]
             resource = request.split()[1]
-            print("Method: "  method)
+            print("Method: " + method)
             print("Resource: " + resource)
             return(method, resource)
         except:
             return('', '')
 
     def process(self, parsedRequest):
-        print("Ahí va ---------------")
-        print(parsedRequest[0])
+        print("Parsedrequest[0]" + parsedRequest[0])
         if parsedRequest[0] == 'GET':
-            return ("200 OK", "<html><body><h1>" +
-                              """<form>
-                                    URL:<br>
-                                    <input type="text" name="URL" value="www.realmadrid.com"><br>
-                                    <input type="submit" value="Submit">
-                                 </form>""" +
-                              "</body></html>")
-        else:
-            return("404 NOT Found", "<html><body><h1> Resource NOT Found</h1>" +
-                                    "<p>Usage: localhost:1234/GET/</p></body></html>")                     
+			if parsedRequest[1] == ''
+	            return ("200 OK", "<html><body><h1>" +
+	                              """<form>
+	                                    URL:<br>
+	                                    <input type="text" name="URL" value="www.realmadrid.com"><br>
+	                                    <input type="submit" value="Submit">
+	                                 </form>""" +
+	                              "</body></html>")
+	        else:
+	            return("404 NOT Found", "<html><body><h1> Resource NOT Found</h1>" +
+	                                    "<p>Usage: localhost:1234/GET/</p></body></html>")
 
 if __name__ == "__main__":
     myApp = URL_shortener("localhost", 1234)
