7cff7614de7bf1ad338856b4598b093b63409796
Rodrigo Pacheco Martinez Atienza <rpacheco@alpha48.aulas.gsyc.urjc.es>
2018-03-13 10:16:25 +0100

    Pep8 check. Style improved

--- practica1.py
+++ practica1.py
@@ -18,12 +18,14 @@ NUMBER_URL = {}
 LAST_URL = 0
 FILE_PATH = "./init_urls.txt"
 
+
 def save_in_file(number, url):
     try:
         file = open(FILE_PATH, "a")
         file.write(str(number) + " " + url + "\r\n")
     except:
         exit("Could not open init_urls.txt when it was suppoused to be created")
+
 
 def its_kown_resource(input_str):
     try:
@@ -34,6 +36,7 @@ def its_kown_resource(input_str):
             return 0    # Number not in dictionary
     except:
         return 0        # Not even a number
+
 
 def current_url_links():
     html_code = "<p><h2>Shortened URLS:</h2></p>"
@@ -52,6 +55,7 @@ def check_url(url):
         return(url)
     else:
         return("http://" + url)
+
 
 def add_url(url, already_added):
     global URL_NUMBER, NUMBER_URL, LAST_URL
@@ -81,13 +85,11 @@ class URL_shortener(webapp.webApp):
         if parsedRequest[0] == "GET":
             if parsedRequest[1] == "/":
                 return ("200 OK", "<html><body><h1>" +
-	                              """<form method=post accept-charset="utf-8">
-	                                 URL:<br>
-	                                 <input type="text" name="URL" value="www.realmadrid.com"><br>
-	                                 <input type="submit" value="Submit">
-	                                 </form>""" +
-                                     current_url_links() +
-	                               "</body></html>")
+	                    """<form method=post accept-charset="utf-8">URL:<br>
+                        <input type="text" name="URL" value="www.realmadrid.com"><br>
+                        <input type="submit" value="Submit"></form>""" +
+                        current_url_links() +
+                        "</body></html>")
             elif its_kown_resource(parsedRequest[1]):
                 return("301 Moved Permanetly", "<html><body>" +
                                                "<head><meta http-equiv=\"refresh\" content=\"0;URL=" +
@@ -95,7 +97,7 @@ class URL_shortener(webapp.webApp):
                                                "\"/></head></body></html>")
             else:
                 return("404 NOT Found", "<html><body><h1> Resource NOT Found</h1>" +
-	                                    "<p>Go to <a href=http://localhost:1234/> home page</a></h1>" +
+                                        "<p>Go to <a href=http://localhost:1234/> home page</a></h1>" +
                                         " to check URLs already shortened</p></body></html>")
         elif parsedRequest[0] == "POST" and len(parsedRequest[2]) > 0:
             checked = check_url(parsedRequest[2])
@@ -115,8 +117,8 @@ if __name__ == "__main__":
         file = open(FILE_PATH, "r")
         for line in file:
             try:
-                LAST_URL = int(line.split()[0]) - 1 # -1 because in add_url number is increased by 1
-                add_url(line.split()[1], 1)         # 1 Because URL already in .txt file
+                LAST_URL = int(line.split()[0]) - 1  # -1 because in add_url number is increased by 1
+                add_url(line.split()[1], 1)          # 1 Because URL already in .txt file
             except:
                 exit("init_urls.txt format not supported. Use number url")
     else:
