1b562fe127c81329909fdd5be85b9f409b64a431
Rodrigo_P <r.pachecom@alumnos.urjc.es>
2018-03-13 23:57:15 +0100

    Last version. Changes to appearance and bug when saving to file solved

--- practica1.py
+++ practica1.py
@@ -7,8 +7,14 @@ r.pachecom @ alumnos.urjc.es
 SAT subject (Universidad Rey Juan Carlos)
 """
 
-redirection = "<a href={}>{}</a>"
-init_page = "localhost:1234/"
+hyperlink = """<a href={}>{}</a>"""
+form = """<html><body><h1>
+        <form method=post accept-charset="utf-8">URL:<br>
+        <input type="text" name="URL" value="www.realmadrid.com"><br>
+        <input type="submit" value="Submit"></form> {}
+        </body></html>"""
+
+init_page = "http://localhost:1234/"
 
 import webapp
 import os.path
@@ -18,16 +24,13 @@ from urllib.parse import unquote
 
 URL_NUMBER = collections.OrderedDict()
 NUMBER_URL = collections.OrderedDict()
-LAST_URL = 0
+LAST_URL = -1                               # It will be earlier incremented tan assigned (Start at 0)
 FILE_PATH = "./init_urls.csv"
 
 
 def save_in_file(number, url):
-    try:
-        file = open(FILE_PATH, "a")
-        file.write(str(number) + "," + url + "\r\n")
-    except:
-        exit("Could not open init_urls.txt when it was suppoused to be created")
+    file = open(FILE_PATH, "a")
+    file.write(str(number) + "," + url + "\r\n")
 
 
 def its_kown_resource(input_str):
@@ -43,7 +46,7 @@ def its_kown_resource(input_str):
 
 def current_url_links():
     html_code = "<p><h2>Shortened URLS:</h2></p>"
-    if LAST_URL > 0:
+    if LAST_URL > -1:
         for url in URL_NUMBER:
             html_code += ("<p><a href=" + url + ">" + str(URL_NUMBER[url]) + "</a>" + " -- " +
                           "<a href=" + url + ">" + url + "</a></p>")
@@ -62,6 +65,8 @@ def check_url(url):
 
 def add_url(url, already_added):
     global URL_NUMBER, NUMBER_URL, LAST_URL
+    if not(already_added):
+        url = url + "\n"
     if url in URL_NUMBER:
         return(url, str(URL_NUMBER[url]))
     else:
@@ -87,12 +92,7 @@ class URL_shortener(webapp.webApp):
     def process(self, parsedRequest):
         if parsedRequest[0] == "GET":
             if parsedRequest[1] == "/":
-                return ("200 OK", "<html><body><h1>" +
-	                    """<form method=post accept-charset="utf-8">URL:<br>
-                        <input type="text" name="URL" value="www.realmadrid.com"><br>
-                        <input type="submit" value="Submit"></form>""" +
-                        current_url_links() +
-                        "</body></html>")
+                return ("200 OK", form.format(current_url_links()))
             elif its_kown_resource(parsedRequest[1]):
                 return("302 Redirect", "<html><body>" +
                                        "<head><meta http-equiv=\"refresh\" content=\"0;URL=" +
@@ -100,20 +100,21 @@ class URL_shortener(webapp.webApp):
                                        "\"/></head></body></html>")
             else:
                 return("404 NOT Found", "<html><body><h1> Resource NOT Found</h1>" +
-                                        "<p>Go to <a href=http://localhost:1234/> home page</a></h1>" +
-                                        " to check URLs already shortened</p></body></html>")
+                                        hyperlink.format(init_page, "Back to start page") +
+                                        "</body></html>")
         elif parsedRequest[0] == "POST" and len(parsedRequest[2]) > 0:
             checked = check_url(parsedRequest[2])
             added = add_url(checked, 0)         # 0 because URL not in .txt file yet
-            return("200 OK", "<html><body><h1>Shortened URL: </h1>" +
-                             "<a href=" + added[0] + ">" + added[1] + "</a></h1>" + " -- "
-                             "<a href=" + added[0] + ">" + added[0] + "</a></h1>" +
-                             "<p><a href=http://localhost:1234/>Back to start page</a></h1></p>" +
+            redirURL = init_page + added[1]
+            return("200 OK", "<html><body><h1>Shortened URL: </h1>" + "<p>"  +
+                             hyperlink.format(redirURL, added[1]) + " -- " +
+                             hyperlink.format(added[0], added[0]) + "</p>" +
+                             hyperlink.format(init_page, "Back to start page") +
                              "</body></html>")
         else:
             return("404 NOT Found", "<html><body><h1> Resource NOT Found</h1>" +
                                     "<p>Usage: localhost:1234/number</p>" +
-                                    redirection.format({init_page, "Click to start page"}) +
+                                    hyperlink.format(init_page, "Click to start page") +
                                     "</body></html>")
 
 if __name__ == "__main__":
